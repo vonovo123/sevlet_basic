089b30d928d942a2c601efd8d9c5a0302b72e661
dynamic_kwon <vonovo123@naver.com>
2025-10-22 17:21:30 +0900

    second commit

---
+++ src/main/java/com/hello/sevlet/SevletApplication.java
@@ -0,0 +1,15 @@
+package com.hello.sevlet;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan // 서블릿 자동등록
+@SpringBootApplication
+public class SevletApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SevletApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/hello/sevlet/basic/HelloServlet.java
@@ -0,0 +1,25 @@
+package com.hello.sevlet.basic;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("req = " + req);
+        System.out.println("resp = " + resp);
+        String userName = req.getParameter("name");
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("utf-8");
+        resp.getWriter().write("hello " + userName);
+    }
+}
